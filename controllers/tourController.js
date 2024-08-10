969a34d42ff829469d0a8e249f2c362e5d0910ec
AnB School 0020 <anbschool0020@AnBs-Mac-mini-19.local>
2024-08-10 11:19:11 +0700

    making api better: data filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -39,11 +39,19 @@ exports.getAllTours = async (req, res) => {
   //     tours
   //   }
   // });
-
+  
   
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = req.query;
+    const excludedFields = ['page', 'sort', 'limit', 'fields']
 
+    excludedFields.forEach(el => delete queryObj[el]);
+    const query = Tour.find(req.query);
+    // REXECUTE THE QUERY 
+    const tours = await query
+
+    //SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
