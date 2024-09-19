d6e1908b0d6cc5ed68956a0678a68b5874f99788
Pann Senglay <senglaypann@gmail.com>
2024-09-19 10:48:31 +0700

    feat: add a tesing custom error handling

--- app.js
+++ app.js
@@ -26,10 +26,27 @@ app.use('/api/v1/users', userRouter);
 
 // ROUTES ERROR HANDLING
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Cannnot find ${req.originalUrl} on this server!`
-  })
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Cannnot find ${req.originalUrl} on this server!`
+  // });
+  // next();
+
+  const err =  new Error(`Cannot find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+  next();
 });
 
 
