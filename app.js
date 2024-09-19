38c7c946818d45996763925fd04f39e74381e976
Pann Senglay <senglaypann@gmail.com>
2024-09-19 10:19:53 +0700

    feat: added routing error handling

--- app.js
+++ app.js
@@ -15,12 +15,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
-
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   console.log(`req sent at ${ req.requestTime}`)
   next();
@@ -29,5 +23,14 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// ROUTES ERROR HANDLING
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Cannnot find ${req.originalUrl} on this server!`
+  })
+});
+
 
 module.exports = app;
