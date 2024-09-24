013aeaf4910980c2c18187046bf8d35574c08a99
Pann Senglay <senglaypann@gmail.com>
2024-09-24 14:41:18 +0700

    refactor: use corss-env instead

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const dotenv = require('dotenv');
 const app = require('./app');
 
+process.on('uncaughtException', err => {
+  console.log("UNCAUGHT EXCEPTION! SHUTTINGDOWN...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
+
+console.log('MODE: ', process.env.NODE_ENV);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
