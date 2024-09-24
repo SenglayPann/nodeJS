5883096291665c8035962ebca8bc7c8217986378
Pann Senglay <senglaypann@gmail.com>
2024-09-24 16:11:53 +0700

    feat: added userModel

---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const validtor = require('validator');
+// name, email, phoo, password, passwordConfirm
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us yuour name']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email address'],
+    unique: true,
+    lowercase: true,
+    validate: [validtor.isEmail, 'Please provide a valid email adrress']
+  },
+  photo: {
+    type: String,
+    required: false,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8
+  },
+  consfirmPassword: {
+    type: String,
+    required: [true, 'Please confirm your password']
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
