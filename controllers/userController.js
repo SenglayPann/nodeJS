ad6b09e9a4122eac7f2d624d9b5719b59f5fddbd
Pann Senglay <senglaypann@gmail.com>
2024-10-18 18:13:34 +0700

    fead added update me controlling function to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,7 @@
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
+const createSendToken = require('../utils/createSendToken');
+
 exports.getAllUsers = (req, res) => {
   res.status(500).json({
     status: 'error',
@@ -28,3 +32,19 @@ exports.deleteUser = (req, res) => {
     message: 'This route is not yet defined!'
   });
 };
+
+exports.updateMe = catchAsync( async (req, res, next) => {
+  // 1) CREATER AN ERROR IF USER POSTS PASSWORD DATA
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(new AppError('This Route is not for password updates. Please use updateMyPassword route.'));
+  };
+
+  // 2) UPDATE USER DOCUMENT
+  const user = req.user;
+  const { name } = req.body
+  user.name = name;
+
+  await user.save({ validateBeforeSave: false });
+
+  createSendToken(user, 201, res);
+});
---
+++ utils/createSendToken.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+const signToken = id => {
+  const secret = process.env.JWT_SECRET
+  const expiresIn = process.env.JWT_EXPIRES_IN
+
+  return  jwt.sign({ id }, secret, {expiresIn: expiresIn});
+};
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user
+    }
+  });
+};
+
+module.exports = createSendToken;
