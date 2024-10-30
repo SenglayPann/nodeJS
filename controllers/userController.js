8c5c4206a257dc048158a1427ecfc05383bd8c89
Pann Senglay <senglaypann@gmail.com>
2024-10-30 16:20:54 +0700

    feat: added getAllUsers Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,20 @@
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 const createSendToken = require('../utils/createSendToken');
+const User = require('../models/userModel');
 
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users
+    }
   });
-};
+});
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
