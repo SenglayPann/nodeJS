bbb4740f7e552c03618cbee9d23e09a213f0198a
Pann Senglay <senglaypann@gmail.com>
2024-10-30 18:04:09 +0700

    feat: added sending coockie to client and express-rate-limit

--- utils/createSendToken.js
+++ utils/createSendToken.js
@@ -1,4 +1,13 @@
+const exp = require('constants');
 const jwt = require('jsonwebtoken');
+
+const cookieOptions = {
+  expire: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+  ),
+  httpOnly: true
+}
+
+if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
 const signToken = id => {
   const secret = process.env.JWT_SECRET
@@ -9,6 +18,10 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
