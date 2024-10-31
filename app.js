77c0d22cf5569defd341d1ceb9b5739c364d297d
Pann Senglay <senglaypann@gmail.com>
2024-10-31 10:02:55 +0700

    feat: used mongoSanitize and xss

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const appError = require('./utils/appError');
 const errorHandler = require('./controllers/errorController');
@@ -28,6 +30,12 @@ app.use('/api', limiter);
 
 // BODY PARSING, READING DATA FROM BODY REQUEST
 app.use(express.json({ limit: '10kb' }));
+
+// DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
+app.use(mongoSanitize());
+
+// DATA SANITIZATION AGAINST XSS
+app.use(xss());
 
 // SERVING STATIC FILES
 app.use(express.static(`${__dirname}/public`));
