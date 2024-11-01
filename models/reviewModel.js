313ca8a067d23f53f244334c3140d9cfd4831c10
Pann Senglay <senglaypann@gmail.com>
2024-11-01 18:21:51 +0700

    feat: removed tour populate from review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -32,10 +32,14 @@ const reviewSchema = new mongoose.Schema({
 });
 
 reviewSchema.pre(/^find/, function(next) {
+  // this.populate([{
+  //   path: 'tour',
+  //   select: 'name price -guides'
+  // },{
+  //   path: 'user',
+  //   select: 'name photo -_id'
+  // },
   this.populate([{
-    path: 'tour',
-    select: 'name price -guides'
-  },{
     path: 'user',
     select: 'name photo -_id'
   },
