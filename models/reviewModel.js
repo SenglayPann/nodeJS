7070c283c77340b744494e301eddbf3ba1b2515d
Pann Senglay <senglaypann@gmail.com>
2024-11-01 15:35:39 +0700

    fea: populated reviews references

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -31,6 +31,19 @@ const reviewSchema = new mongoose.Schema({
   toObject: {virtuals: true}
 });
 
+reviewSchema.pre(/^find/, function(next) {
+  this.populate([{
+    path: 'tour',
+    select: 'name price -_id -guides'
+  },{
+    path: 'user',
+    select: 'name photo -_id'
+  },
+]);
+  next();
+});
+
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
