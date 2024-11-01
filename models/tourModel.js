9e3e19b1c27a0caecd91f098df3d820ea9dbc32e
Pann Senglay <senglaypann@gmail.com>
2024-11-01 11:11:44 +0700

    feat: added pre save middleware that when create a new tour bye adding actual user

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose =  require('mongoose');
 const slugify = require('slugify');
 const database = require('../config/database');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -152,6 +153,12 @@ tourSchema.post(/^find/, function(doc, next) {
 // AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function(next) {
   this.pipeline().unshift({ $match: { secretTour: { $ne: true} } });
+  next();
+});
+
+tourSchema.pre('save', async function(next) {
+  const guidesPromises = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
