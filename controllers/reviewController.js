021b7f67208b82f60ef29e136277982e4b506527
Pann Senglay <senglaypann@gmail.com>
2024-11-01 14:39:05 +0700

    feat: added update function for review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -30,3 +30,50 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
     }
   });
 });
+
+exports.getReview = catchAsync(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(new AppError(`No review was found for id ${req.params.id}`, 404));
+  }     
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      review
+    }
+  });
+});
+
+
+exports.deleteReview = catchAsync(async (req, res, next) => {
+  const review = await Review.findByIdAndDelete(req.params.id);
+
+  if (!review) {
+    return next(new AppError(`No review was found for id ${req.params.id}`, 404));
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
+
+exports.updateReview = catchAsync(async (req, res, next) => {
+  const review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  if (!review) {
+    return next(new AppError(`No review was found for id ${req.params.id}`, 404));
+  }     
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      review
+    }
+  });
+});
