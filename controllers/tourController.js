98b0c84af2f7260d3fa4b148773de5e148ce911d
Pann Senglay <senglaypann@gmail.com>
2024-11-03 15:26:02 +0700

    refactor: refactor fetTour by using factory getOne method

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -31,22 +31,7 @@ exports.getAllTours = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res, next) => {
-
-  const tour = await Tour.findById(req.params.id)
-  .populate('reviews');
-
-  if(!tour) {
-    return next(new AppError(`No tour was found fo the id ${req.params.id}`), 404);
-  };
-  
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour
-    }
-  });
-});
+exports.getTour = factory.getOne(Tour, 'reviews');
 
 exports.createTour = factory.createOne(Tour);
 
