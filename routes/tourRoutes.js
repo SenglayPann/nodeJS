d602c0f9ce85a3f9f45a7fa86d921c19ddcd81bb
Pann Senglay <senglaypann@gmail.com>
2024-11-03 16:37:01 +0700

    chore: added authorization to routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -15,12 +15,12 @@ router
 
 router
   .route('/monthly-plan/:year')
-  .get(tourController.getMonthlyPlan);
+  .get(authController.protect, authController.restrictTo('admin', 'lead-guide', 'guide'), tourController.getMonthlyPlan);
 
 router
   .route('/')
-  .get(authController.protect, tourController.getAllTours)
-  .post(authController.protect, authController.restrictTo('admin', 'user'), tourController.createTour);
+  .get(tourController.getAllTours)
+  .post(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.createTour);
 
 router
   .route('/:id')
