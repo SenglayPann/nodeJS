8d029bc7818f8fb08f676729074cc0cfecff108d
Pann Senglay <senglaypann@gmail.com>
2024-11-03 13:27:59 +0700

    feat: addeds createOne in the factory function

--- factories/handlerFactory.js
+++ factories/handlerFactory.js
@@ -29,3 +29,12 @@ exports.updateOne = Model => catchAsync(async (req, res, next) => {
     data: doc
   });
 });
+
+exports.createOne = Model => catchAsync(async (req, res, next) => {
+  const doc = await Model.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: doc
+  });
+});
