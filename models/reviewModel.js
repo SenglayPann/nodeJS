ed41b809f9cf854f73c0b8f923fec2c93d502dff
Pann Senglay <senglaypann@gmail.com>
2024-11-04 13:37:22 +0700

    feat: added handling tour agverage rating and rating quantity when update and delete

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -72,6 +72,17 @@ reviewSchema.post('save', function() {
   this.constructor.calAverageRatings(this.tour);
 });
 
+// findByIdAndUpdate
+// findByIdAndDelete
+reviewSchema.pre(/^findOneAnd/, async function(next) {
+  this.r = await this.findOne();
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function() {
+  await this.r.constructor.calAverageRatings(this.r.tour);
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
