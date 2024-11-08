c880bdeae103eeae7db8d7d303ab641c750fc905
Pann Senglay <senglaypann@gmail.com>
2024-11-08 11:24:50 +0700

    feat: updated update me capabling upate user photo

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const catchAsync = require("../utils/catchAsync");
 const createSendToken = require('../utils/createSendToken');
 const User = require('../models/userModel');
 const factory = require('../factories/handlerFactory');
+const filterObj = require('../utils/filterObj');
 
 exports.getAllUsers = factory.getAll(User);
 
@@ -25,14 +26,23 @@ exports.updateMe = catchAsync( async (req, res, next) => {
     return next(new AppError('This Route is not for password updates. Please use updateMyPassword route.', 400));
   };
 
-  // 2) UPDATE USER DOCUMENT
-  const user = req.user;
-  const { name } = req.body
-  user.name = name;
+  // 2) FILTERED OUT UNWANTED FIELDS NAME THAT ARE NOT ALLOWED TO BE UPDATED
+  const filteredBody = filterObj(req.body, 'name', 'email');
 
-  await user.save({ validateBeforeSave: false });
+  if (req.file) filteredBody.photo = req.file.filename;
 
-  createSendToken(user, 201, res);
+  // 3) UPDATE USER DOCUMENT
+  const updatedUser = await User.findByIdAndUpdate(req.user._id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
 });
 
 exports.deleteMe = catchAsync(async (req, res, next) => {
