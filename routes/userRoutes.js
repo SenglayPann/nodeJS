ce35b0948292704fcc00ef4dab6a5b1e218848b0
Pann Senglay <senglaypann@gmail.com>
2024-11-08 11:01:20 +0700

    feat: added multer configuration

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const AppError = require('../utils/appError'); 
 const multer = require('multer');
 
-const upload = multer({ dest: 'public/img/users'});
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/img/users');
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split('/')[1];
+    cb(null, 'user-' + req.user.id + '-' + Date.now() + '.' + ext);
+  }
+});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an Image! Please provide an image', 400), false);
+  };
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter
+});
 
 const router = express.Router();
 
