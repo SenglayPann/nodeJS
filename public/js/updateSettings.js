dca2b54431092fabe8c174376d51c5899a676b1b
Pann Senglay <senglaypann@gmail.com>
2024-11-10 11:51:44 +0700

    feat: use form data to upload photo

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -4,7 +4,6 @@ import { showAlert } from './alerts';
 
 // type is either 'password' or 'data'
 export const updateSettings = async (data, type) => {
-  console.log(data)
   try {
     const url =
       type === 'password'
