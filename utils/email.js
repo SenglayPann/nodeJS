abdfa83cee7f6282d2ed1f03b87fa466776dcf69
Pann Senglay <senglaypann@gmail.com>
2024-11-10 13:50:52 +0700

    feat: update sendEmail configuration by creating class

--- utils/email.js
+++ utils/email.js
@@ -1,27 +1,91 @@
 const nodemailer = require('nodemailer');
 
-const sendEmail = async (options) => {
-  // 1) CREATE A TRANSPORTER
-  const transporter = nodemailer.createTransport({
-    // service: 'Gmail',
-    host: process.env.EMAIL_HOST,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Senglay Pann <${process.env.EMAIL_FROM}>`;
+  }
+  
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // Sendgrid
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD
+        }
+      });
     }
-  });
 
-  // 2) DEFINE  THE EMAIL
-  const mailOptions = {
-    from: 'senglaypann@mail.com',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html:
-  };
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT, 
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+  }
 
-  // 3) ACUALLY SEND THE EMAIL
-  await transporter.sendMail(mailOptions);
+  async send(template, subject) {
+    // 1) Render HTML based on a pug template
+    const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject
+    });
+
+    // 2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html)
+    };
+
+    // 3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours Family!');
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes)'
+    );
+  }
+
 }
 
-module.exports = sendEmail;
+// const sendEmail = async (options) => {
+//   // 1) CREATE A TRANSPORTER
+//   const transporter = nodemailer.createTransport({
+//     // service: 'Gmail',
+//     host: process.env.EMAIL_HOST,
+//     auth: {
+//       user: process.env.EMAIL_USERNAME,
+//       pass: process.env.EMAIL_PASSWORD
+//     }
+//   });
+
+//   // 2) DEFINE  THE EMAIL
+//   const mailOptions = {
+//     from: 'senglaypann@mail.com',
+//     to: options.email,
+//     subject: options.subject,
+//     text: options.message,
+//     // html:
+//   };
+
+//   // 3) ACUALLY SEND THE EMAIL
+//   await transporter.sendMail(mailOptions);
+// }
+
+// module.exports = sendEmail;
