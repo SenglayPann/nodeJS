e93e428642afb873add4a25d6ac0a395f1546380
Pann Senglay <senglaypann@gmail.com>
2024-11-13 09:13:10 +0700

    chore: uncommand stripe.js

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,24 +1,23 @@
 /* eslint-disable */
 import axios from 'axios';
 import { showAlert } from './alerts';
-// const stripe = Stripe('pk_test_51QJaghBeVFOMWJTdbdunGcBQUNeo519BgjN2jQ5JQ9yeuanbV9kxvJo92kvvhMZJ0ALoXbM4xXgHWj2uyiYrYLSf00YsJzCRaZ');
+const stripe = Stripe('pk_test_51QJaghBeVFOMWJTdbdunGcBQUNeo519BgjN2jQ5JQ9yeuanbV9kxvJo92kvvhMZJ0ALoXbM4xXgHWj2uyiYrYLSf00YsJzCRaZ');
 
 export const bookTour = async tourId => {
 
-  console.log(tourId)
-  // try {
-  //   // 1) Get checkout session from API
-  //   const session = await axios(
-  //     `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-  //   );
-  //   console.log(session);
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    console.log(session);
 
-  //   // 2) Create checkout form + chanre credit card
-  //   await stripe.redirectToCheckout({
-  //     sessionId: session.data.session.id
-  //   });
-  // } catch (err) {
-  //   console.log(err);
-  //   showAlert('error', err);
-  // }
+    // 2) Create checkout form + chanre credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
 };
